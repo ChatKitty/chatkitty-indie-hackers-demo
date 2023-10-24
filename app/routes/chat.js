30a6d27db67cb679c8189c2f411608d55932df58
Aaron Nwabuoku <aaron@chatkitty.com>
2023-10-24 05:19:17 -0400

    Replace @chatkitty/chat-ui-ember with @chatkitty/ui

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,4 +1,6 @@
 import Route from '@ember/routing/route';
+import { scheduleOnce } from '@ember/runloop';
+import { loadChatUi } from '@chatkitty/ui';
 
 export default class ChatRoute extends Route {
   model(params, transition) {
@@ -7,49 +9,65 @@ export default class ChatRoute extends Route {
 
     const userMap = {
       csallen: {
-        display_name: 'Courtland Allen',
-        display_picture:
+        displayName: 'Courtland Allen',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_ibTLPyjwVebnZjMGKvz6ztarnuV2.webp?1694472104876',
       },
       channingallen: {
-        display_name: 'Channing Allen',
-        display_picture:
+        displayName: 'Channing Allen',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_PWairgiOpneHvkGJri7RVbtORKI2.webp?1689703300131',
       },
       HelpStay: {
-        display_name: 'Shay',
-        display_picture:
+        displayName: 'Shay',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_tDOUNPCt56OY5voGhwJxT1iFnj82.webp?1691251548769',
       },
       goutham8: {
-        display_name: 'Goutham',
-        display_picture:
+        displayName: 'Goutham',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_jk4udTil1vUsZWWZgH9W6emiI0U2.webp?1683385735709',
       },
       gwesterman: {
-        display_name: 'Gordon Westerman',
-        display_picture:
+        displayName: 'Gordon Westerman',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_61tEWxEjhzVD42Gd5Lx6PQCUhiW2.webp?1693919932732',
       },
       aerovulpe: {
-        display_name: 'Aaron Nwabuoku',
-        display_picture:
+        displayName: 'Aaron Nwabuoku',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_Fpnxr5j7s0YXVCFxpsZWs0oN3Ac2.webp?1695569564831',
       },
       August_BK: {
-        display_name: 'Raymond Chen',
-        display_picture:
+        displayName: 'Raymond Chen',
+        displayPicture:
           'https://storage.googleapis.com/indie-hackers.appspot.com/avatars/96x96_B66BJ8EXJEZZZnKmGVwBnd31ChL2.webp',
       },
     };
 
-    return {
+    const model = {
       username: currentUser,
       route: {
         name: 'direct-messages',
-        chat_users: [username],
+        chatUsers: [username],
       },
       profile: userMap[currentUser],
     };
+
+    scheduleOnce('afterRender', this, loadChat);
+
+    function loadChat() {
+      loadChatUi({
+        widgetId: 'UoczKLBBvEyBFEF7',
+        container: {
+          height: '100%',
+        },
+        username: model.username,
+        profile: model.profile,
+        route: model.route,
+      });
+    }
+
+    return model;
   }
 }
